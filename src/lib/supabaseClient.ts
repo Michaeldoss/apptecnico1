5611b2cb07601652fa5d766c306cb2414151f0f6
Michaeldoss <dossgroupequipa@gmail.com>
2025-07-23 22:19:20 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zvokljetwvykcpolyqgp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2b2tsamV0d3Z5a2Nwb2x5cWdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0ODc1MzYsImV4cCI6MjA2NzA2MzUzNn0.qd3pJLHgbnKcyMvDG7iynYR58lIcYoso0S5I7BvlTZE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
